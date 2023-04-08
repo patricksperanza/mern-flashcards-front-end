63b77dca7527c368c688f726a033c22d05f4423e
Patrick Speranza <patrick.m.speranza@gmail.com>
2023-04-08 09:28:02 -0500

    add delete function

--- src/components/QuestionList.jsx
+++ src/components/QuestionList.jsx
@@ -4,9 +4,20 @@ import "./QuestionList.css"
 import { AiOutlinePlus, AiOutlineEdit } from "react-icons/ai"
 import { BsTrash3 } from "react-icons/bs"
 import { Link } from "react-router-dom"
+import axios from "axios"
 
 const QuestionList = () => {
   const [questionData, setQuestionData] = useOutletContext()
+
+  const deleteQuestion = (id) => {
+    console.log(id)
+    axios
+      .delete("https://mern-flashcards-app.herokuapp.com/flashcards" + id)
+      .then((res) => {
+        console.log(res.data)
+      })
+      .catch((err) => console.log(err))
+  }
   return (
     <div className="container">
       <Link to="/add" className="add-link">
@@ -18,14 +29,17 @@ const QuestionList = () => {
       <div className="list">
         {questionData.map((q) => {
           return (
-            <div key={q.id} className="question">
+            <div key={q._id} className="question">
               <div className="question-text">
                 <h6 className="list-question">{q.question}</h6>
                 <p className="list-answer">{q.answer}</p>
               </div>
               <div className="edit-btns">
                 <AiOutlineEdit className="btn" />
-                <BsTrash3 className="btn" />
+                <BsTrash3
+                  className="btn"
+                  onClick={() => deleteQuestion(q._id)}
+                />
               </div>
             </div>
           )
